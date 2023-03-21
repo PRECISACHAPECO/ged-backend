61bbf528929032927192e47c099aeda1f5d20f6d
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-03-21 18:19:36 -0300

    first commig

--- controllers/atividadeController.js
+++ controllers/atividadeController.js
@@ -1,12 +1,22 @@
 const db = require('../config/db');
 
-class AtividadeController {    
+class AtividadeController {
     getList(req, res) {
         db.query("SELECT atividadeID AS id, nome, status FROM atividade", (err, result) => {
             if (err) {
                 res.status(500).json(err);
             } else {
                 res.status(200).json(result);
+            }
+        })
+    }
+    getData(req, res) {
+        const { id } = req.params
+        db.query("SELECT * FROM atividade WHERE atividadeID = ?", [id], (err, result) => {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                res.status(200).json(result[0]);
             }
         })
     }
@@ -22,9 +32,21 @@ class AtividadeController {
         })
     }
 
+    updateData(req, res) {
+        const { id } = req.params
+        const { nome, status } = req.body
+        db.query("UPDATE atividade SET nome = ?, status = ?  WHERE atividadeID = ?", [nome, status, id], (err, result) => {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                res.status(200).json(result);
+            }
+        })
+    }
+
     deleteData(req, res) {
         const { id } = req.params
-        db.query("DELETE FROM register WHERE registerID = ?", [id], (err, result) => {
+        db.query("DELETE FROM atividade WHERE atividadeID = ?", [id], (err, result) => {
             if (err) {
                 res.status(500).json(err);
             } else {
--- routes/atividadeRoutes.js
+++ routes/atividadeRoutes.js
@@ -1,12 +1,20 @@
-const { Router }  = require('express');
-const atividadeRoutes = Router(); 
+const { Router } = require('express');
+const atividadeRoutes = Router();
 
 const AtividadeController = require('../controllers/atividadeController');
 const atividadeController = new AtividadeController();
 
 atividadeRoutes.get("/atividade", atividadeController.getList);
-atividadeRoutes.post("/atividade", atividadeController.insertData);
+atividadeRoutes.get("/atividade/:id", atividadeController.getData);
+atividadeRoutes.put("/atividade/:id", atividadeController.updateData);
 atividadeRoutes.delete("/atividade/:id", atividadeController.deleteData);
+
+
+
+
+
+
+atividadeRoutes.post("/atividade", atividadeController.insertData);
 // registerRoutes.post("/register/:userID", registerController.create);
 // registerRoutes.get("/register/:registerID", registerController.listiD);
 
