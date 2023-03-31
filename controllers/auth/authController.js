bc098bb447bde6038ddd22aa5e7e876b21070592
Roberto Delavi <roberto.delavy@gmail.com>
2023-03-31 10:04:02 -0300

    Login

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,22 +1,45 @@
 const db = require('../../config/db');
 
+// ** JWT import
+const jwt = require('jsonwebtoken');
+
+// ! These two secrets should be in .env file and not in any other file
+const jwtConfig = {
+    secret: process.env.NEXT_PUBLIC_JWT_SECRET,
+    expirationTime: process.env.NEXT_PUBLIC_JWT_EXPIRATION,
+    refreshTokenSecret: process.env.NEXT_PUBLIC_JWT_REFRESH_TOKEN_SECRET
+}
+
 class AuthController {
     login(req, res) {
         const { email, password } = req.body;
 
-        db.query("SELECT * FROM usuario WHERE email = ? AND password = ?", [email, password], (err, result) => {
-            if (err) {
-                res.status(500).json({ message: err.message });
-            } else {
-                if (result.length > 0) {
-                    res.status(200).json({ message: "Login efetuado com sucesso!" });
-                } else {
-                    res.status(401).json({ message: "Usuário ou senha inválidos!" });
+        let error = {
+            email: ['Algo está errado!']
+        }
+
+        db.query("SELECT * FROM usuario WHERE email = ? AND senha = ?", [email, password], (err, result) => {
+            if (err) { res.status(500).json({ message: err.message }); }
+            // LOGADO COM SUCESSO
+            if (result.length > 0) {
+                const accessToken = jwt.sign({ id: result[0]['usuarioID'] }, jwtConfig.secret, { expiresIn: jwtConfig.expirationTime })
+                const response = {
+                    accessToken,
+                    userData: { ...result[0], senha: undefined }
                 }
+
+                res.status(200).json(response);
+            }
+            // ERRO AO FAZER LOGIN
+            else {
+                error = {
+                    email: ['E-mail ou senha inválidos!']
+                }
+                
+                res.status(400).json(error);
             }
         })
     }
-
 }
 
 module.exports = AuthController;
