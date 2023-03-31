9a146d0b955d257b8c40c9ae798c0ce00fb310fa
Roberto Delavi <roberto.delavy@gmail.com>
2023-03-31 16:27:41 -0300

    Formulário

--- controllers/configuracoes/formularios/fornecedor/fornecedorController.js
+++ controllers/configuracoes/formularios/fornecedor/fornecedorController.js
@@ -3,13 +3,13 @@ const { hasPending, deleteItem } = require('../../../../config/defaultConfig');
 
 class FornecedorController {
 
-    getData(req, res) {
+    async getData(req, res) {
         const functionName = req.headers['function-name'];
         const unidadeID = req.params.id;
 
-        switch(functionName) {
+        switch (functionName) {
             // Obtém cabeçalho do formulário
-            case 'getHeader':                
+            case 'getHeader':
                 const sql1 = `
                 SELECT pf.*, 
                     (SELECT IF(COUNT(*) > 0, 1, 0)
@@ -22,31 +22,124 @@ class FornecedorController {
                     WHERE pf.parFornecedorID = pfu.parFornecedorID AND pfu.unidadeID = ?
                     LIMIT 1), 0) AS obrigatorio            
                 FROM par_fornecedor AS pf 
-                ORDER BY pf.ordem ASC;`
-        
-                db.query(sql1, [unidadeID, unidadeID], (err, result) => {
-                    if (err) {
-                        res.status(500).json(err);
-                    } else {
-                        res.status(200).json(result);
-                    }
-                })
-            break;
+                ORDER BY pf.ordem ASC;`;
+
+                try {
+                    const result1 = await db.promise().query(sql1, [unidadeID, unidadeID]);
+                    res.status(200).json(result1[0]);
+                } catch (err) {
+                    res.status(500).json(err);
+                }
+                break;
 
             // Obtém blocos do formulário
             case 'getBlocks':
-                const sql2 = `SELECT * FROM par_fornecedor_bloco WHERE unidadeID = ?`
+                try {
+                    const blocks = [];
+                    const sql2 = `SELECT * FROM par_fornecedor_bloco WHERE unidadeID = ? ORDER BY ordem ASC`;
+                    const [result2] = await db.promise().query(sql2, [unidadeID]);
 
-                db.query(sql2, [unidadeID], (err, result) => {
-                    if (err) {
-                        res.status(500).json(err);
-                    } else {
-                        res.status(200).json(result);
+                    const sql3 = `
+                    SELECT a.*, 
+                        (SELECT IF(COUNT(*) > 0, 1, 0)
+                        FROM par_fornecedor_bloco_atividade AS pfba 
+                        WHERE pfba.atividadeID = a.atividadeID AND pfba.parFornecedorBlocoID = ? AND pfba.unidadeID = ?) AS checked
+                    FROM atividade AS a 
+                    ORDER BY a.nome ASC;`;
+
+                    // Varre bloco
+                    for (const item of result2) {
+                        const [result3] = await db.promise().query(sql3, [item.parFornecedorBlocoID, unidadeID]);
+
+                        const objData = {
+                            dados: item,
+                            atividades: result3,
+                            categrias: [
+                                {
+                                    id: 1,
+                                    nome: 'Fabricante',
+                                    checked: 1
+                                },
+                                {
+                                    id: 2,
+                                    nome: 'Importador',
+                                    checked: 0
+                                }
+                            ],
+                        };
+
+                        blocks.push(objData);
                     }
-                })
-            break;
+
+                    res.status(200).json(blocks);
+                } catch (err) {
+                    res.status(500).json(err);
+                }
+                break;
         }
     }
+
+    // getData(req, res) {
+    //     const functionName = req.headers['function-name'];
+    //     const unidadeID = req.params.id;
+
+    //     switch (functionName) {
+    //         // Obtém cabeçalho do formulário
+    //         case 'getHeader':
+    //             const sql1 = `
+    //             SELECT pf.*, 
+    //                 (SELECT IF(COUNT(*) > 0, 1, 0)
+    //                 FROM par_fornecedor_unidade AS pfu 
+    //                 WHERE pf.parFornecedorID = pfu.parFornecedorID AND pfu.unidadeID = ?
+    //                 LIMIT 1) AS mostra,
+
+    //                 COALESCE((SELECT pfu.obrigatorio
+    //                 FROM par_fornecedor_unidade AS pfu 
+    //                 WHERE pf.parFornecedorID = pfu.parFornecedorID AND pfu.unidadeID = ?
+    //                 LIMIT 1), 0) AS obrigatorio            
+    //             FROM par_fornecedor AS pf 
+    //             ORDER BY pf.ordem ASC;`
+
+    //             db.query(sql1, [unidadeID, unidadeID], (err, result) => {
+    //                 if (err) {
+    //                     res.status(500).json(err);
+    //                 } else {
+    //                     res.status(200).json(result);
+    //                 }
+    //             })
+    //             break;
+
+    //         // Obtém blocos do formulário
+    //         case 'getBlocks':
+    //             try {
+    //                 const blocks = [];
+    //                 const sql2 = `SELECT * FROM par_fornecedor_bloco WHERE unidadeID = ?`;
+    //                 const result = db.query(sql2, [unidadeID]);
+
+    //                 const sql3 = `SELECT * FROM par_fornecedor_bloco_atividade AS pfba JOIN atividade AS a ON (pfba.atividadeID = a.atividadeID) WHERE pfba.parFornecedorBlocoID = ? ORDER BY a.nome ASC`;
+
+    //                 // Varre bloco
+    //                 for (const item of result) {
+    //                     const result2 = db.query(sql3, [item.parFornecedorBlocoID]);
+
+    //                     console.log('==> ', result2[0]['nome'])
+
+    //                     // inserir em blocks o result e result2 dentro do array atividades em blocks 
+    //                     const novoObjeto = {
+    //                         dados: item,
+    //                         atividades: result2,
+    //                     };
+
+    //                     blocks.push(novoObjeto);
+    //                 }
+
+    //                 res.status(200).json(blocks);
+    //             } catch (err) {
+    //                 res.status(500).json(err);
+    //             }
+    //             break;
+    //     }
+    // }
 
     updateData(req, res) {
         const unidadeID = req.params.id;
