655c26a8337f4dcc0111903ce63dc3cc77100d84
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-04-13 09:42:08 -0300

    ajustado rotas e função que gera relatorio no front

--- reports/index.js
+++ reports/index.js
@@ -4,10 +4,10 @@ const urlBase = '/api';
 
 // Fornecedor
 const { reportFornecedor } = require('../reports/fornecedor/generate');
-routerReports.post('/relatorio/fornecedor/', reportFornecedor);
+routerReports.post(`${urlBase}/relatorio/fornecedor/`, reportFornecedor);
 
 // Recepção
 const { reportRecepcao } = require('../reports/recepcao/generate');
-routerReports.post('/relatorio/recepcao/', reportRecepcao);
+routerReports.post(`${urlBase}/relatorio/recepcao/`, reportRecepcao);
 
 module.exports = routerReports;
