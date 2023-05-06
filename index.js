cd982321bc674b26ef4bc8dc8b100a46fb5294ee
Roberto Delavi <roberto.delavy@gmail.com>
2023-05-06 10:43:14 -0300

    Habilitado dependencias e rotas dos relatórios novamente

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require("./routes");
-// const routerReports = require("./reports");
+const routerReports = require("./reports");
 app.use(express.json());
 app.use(cors());
 app.use(routes);
-// app.use(routerReports);
+app.use(routerReports);
 
 app.listen(3333, (req, res) => {
     console.log('Server is running on port 3333');
