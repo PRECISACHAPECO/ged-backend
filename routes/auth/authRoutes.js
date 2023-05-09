536e919c8681e97b26383675404d1fcca1cce0e9
Roberto Delavi <roberto.delavy@gmail.com>
2023-05-09 14:16:19 -0300

    Correções terça-feira

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -5,8 +5,8 @@ const AuthController = require('../../controllers/auth/authController');
 const authController = new AuthController();
 
 //* Cadastro do fornecedor
-// const routeRegister = '/registro';
-// authRoutes.post(`${routeRegister}`, authController.register);
+const routeRegister = '/registro';
+authRoutes.post(`${routeRegister}`, authController.register);
 
 //* Login da fábrica
 const route = '/login';
@@ -14,7 +14,7 @@ authRoutes.post(`${route}`, authController.login);
 authRoutes.get(`${route}`, authController.getAvailableRoutes);
 
 //* Login dor fornecedor 
-// const routeFornecedor = '/login-fornecedor';
-// authRoutes.post(`${routeFornecedor}`, authController.loginFornecedor);
+const routeFornecedor = '/login-fornecedor';
+authRoutes.post(`${routeFornecedor}`, authController.loginFornecedor);
 
 module.exports = authRoutes;
