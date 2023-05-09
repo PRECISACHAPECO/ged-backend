de30b9ef7fa931d9bb6dfc421895fa1ffc6c8fe0
Roberto Delavi <roberto.delavy@gmail.com>
2023-05-09 13:40:28 -0300

    Correções pastas routes

--- config/db.js
+++ config/db.js
@@ -8,6 +8,11 @@ const db = mysql2.createConnection({
     'user': process.env.DB_USER,
     'password': process.env.DB_PASSWORD,
     'database': process.env.DB_DATABASE,
+
+    // 'host': 'localhost', //process.env.DB_HOST,
+    // 'user': 'gedagroc_user', //process.env.DB_USER,
+    // 'password': 'Jw6!Jr0+Vw4+Lc8#', //process.env.DB_PASSWORD,
+    // 'database': 'gedagroc_ged', //process.env.DB_DATABASE,
 });
 
 module.exports = db;
