d00d639bab697a2d060cea38e034d8f3305b7de5
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-05-22 09:00:26 -0300

    ajustado apenas 1 link email fornecedor

--- email/template/formularios/fornecedor/instructionsNewFornecedor.js
+++ email/template/formularios/fornecedor/instructionsNewFornecedor.js
@@ -4,7 +4,8 @@ const css = require('../../defaults/css');
 require('dotenv/config');
 const urlBase = process.env.BASE_URL;
 
-async function instructionsNewFornecedor(cnpj, unidade) {
+async function instructionsNewFornecedor(cnpj, unidade, haveLogin) {
+    console.log("haveLogin: ", haveLogin);
     // link login e registro enviando cnpj e unidade como parâmetros
     const linkLogin = `${urlBase}/fornecedor/?c=${cnpj}&u=${unidade}`;
     const linkRegistro = `${urlBase}/registro/?c=${cnpj}&u=${unidade}`;
@@ -21,11 +22,21 @@ async function instructionsNewFornecedor(cnpj, unidade) {
     // Conteúdo
     html += `
                 <div class="content">
-                    <h1 class="title">Caro fornecedor,</h1>
-                    <p>Gostaríamos de solicitar que você faça o seu cadastro em nosso sistema para estabelecermos uma parceria comercial. Para isso, pedimos que acesse o link para realizar o primeiro cadastro: </p>
-                    <p><a class="link" href=${linkRegistro}>Primeiro cadastro</a></p>
-                    <p>Caso já tenha realizado o cadastro anteriormente, você pode simplesmente acessar o link de login:</p>
-                    <p><a class="link" href=${linkLogin}>Login</a></p>
+                    <h1 class="title">Caro fornecedor,</h1>`;
+    {
+        if (haveLogin) {
+            html += `
+                <p>acessar link de login:</p>
+                <p><a class="link" href=${linkLogin}>Login</a></p>
+            `;
+        } else {
+            html += `
+                <p>Gostaríamos de solicitar que você faça o seu cadastro em nosso sistema para estabelecermos uma parceria comercial. Para isso, pedimos que acesse o link para realizar o primeiro cadastro: </p>
+                <p><a class="link" href=${linkRegistro}>Cadastro</a></p>
+            `;
+        }
+    }
+    html += `
                     <p>O cadastro em nosso sistema é fundamental para que possamos agilizar o processo de comunicação e facilitar a realização de transações comerciais entre nós. É importante que você preencha todos os campos necessários de forma correta e completa.</p>
                     <p>Se tiver alguma dúvida ou dificuldade durante o processo de cadastro, entre em contato com nossa equipe de suporte, que estará pronta para ajudá-lo.</p>
                     <p>Agradecemos pela sua atenção e estamos ansiosos para iniciar uma parceria bem-sucedida.</p>
