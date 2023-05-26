489d04b9e2bed63b5ba736cb38e0a55d366dd44e
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-05-26 14:18:02 -0300

    ddd

--- app.js
+++ app.js
@@ -17,34 +17,36 @@ app.get('/api/report', async (req, res) => {
         const page = await browser.newPage();
 
         const html = `
-        <html>
-          <head>
-            <style>
-              body {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                font-family: Arial, sans-serif;
-                height: 100vh;
-                margin: 0;
-                padding: 0;
-                background-color: #f0f0f0;
-              }
-              h1 {
-                color: #333;
-              }
-              p {
-                color: #777;
-              }
-            </style>
-          </head>
-          <body>
-            <h1>Relatório</h1>
-            <p>Este é um exemplo de relatório gerado com Headless Chrome.</p>
-          </body>
-        </html>
-      `;
+      <html>
+        <head>
+          <style>
+            body {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              font-family: Arial, sans-serif;
+              height: 100vh;
+              margin: 0;
+              padding: 0;
+              background-color: #f0f0f0;
+            }
+            h1 {
+              color: #333;
+            }
+            p {
+              color: #777;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>Relatório</h1>
+          <p>Este é um exemplo de relatório gerado com Puppeteer.</p>
+          <script>
+            window.open('/api/report-pdf', '_blank');
+          </script>
+        </body>
+      </html>
+    `;
 
         await page.setContent(html);
 
@@ -54,18 +56,18 @@ app.get('/api/report', async (req, res) => {
 
         await browser.close();
 
-        const pdfBuffer = fs.readFileSync(pdfPath);
-        fs.unlinkSync(pdfPath); // Remover o arquivo PDF após lê-lo
-
-        res.set({
-            'Content-Type': 'application/pdf',
-            'Content-Disposition': 'attachment; filename="relatorio.pdf"',
-        });
-        res.send(pdfBuffer);
+        res.set('Content-Type', 'text/html');
+        res.send(html);
     } catch (error) {
         console.error(error);
         res.status(500).send('Erro ao gerar o relatório.');
     }
+});
+
+// Rota para fornecer o arquivo PDF do relatório
+app.get('/api/report-pdf', (req, res) => {
+    const pdfPath = path.join(__dirname, 'relatorio.pdf');
+    res.sendFile(pdfPath);
 });
 
 app.listen(port, () => {
