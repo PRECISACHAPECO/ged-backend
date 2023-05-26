6f365c527969ecbe24975f07243d907ec63bfd76
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-05-26 13:50:21 -0300

    teetete

--- app.js
+++ app.js
@@ -1,15 +1,67 @@
 const express = require('express');
 const cors = require('cors');
+const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
-const routes = require("./routes");
-const routerReports = require("./reports");
+const port = 3333;
+
 app.use(express.json());
 app.use(cors());
-app.use(routes);
-app.use(routerReports);
 
-app.listen(3333, (req, res) => {
-    console.log('Server is running on port 3333');
+// Rota para gerar o relatório
+app.get('/api/report', async (req, res) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        const html = `
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+            }
+            h1 {
+              color: #333;
+            }
+            p {
+              color: #777;
+            }
+          </style>
+        </head>
+        <body>
+        <div style="display: flex; background-color: red;">
+        <h1>Relatório</h1>
+        <p>Este é um exemplo de relatório gerado com Puppeteer.</p>
+        </div>
+        </body>
+      </html>
+    `;
+
+        await page.setContent(html);
+
+        const pdfPath = path.join(__dirname, 'relatorio.pdf'); // Caminho para o arquivo PDF gerado
+
+        await page.pdf({ path: pdfPath, format: 'A4' });
+
+        await browser.close();
+
+        const pdfBuffer = fs.readFileSync(pdfPath);
+        fs.unlinkSync(pdfPath); // Remover o arquivo PDF após lê-lo
+
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="relatorio.pdf"',
+        });
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao gerar o relatório.');
+    }
 });
 
-
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- reports/fornecedorAA/generate.js
+++ reports/fornecedorAA/generate.js
@@ -1,16 +1,8 @@
 const pdf = require('html-pdf');
 const { generateContent } = require('./content');
 
-<<<<<<< HEAD:reports/fornecedorAA/generate.js
 async function reportFornecedor(req, res) {
     const { fornecedorID, unidadeID } = req.query;
-=======
-async function teste(req, res) {
-    let nome = "John"
-    const content = await generateContent(nome);
->>>>>>> d7ccb58f86d3c6b0c8f377e4401b2e90df90fcc8:reports/teste/generate.js
-
-
 
 
     const content = '<h1>Teste</h1>'
