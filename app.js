4e1dd972fbe1fe77345509599f37b5cf66a2971b
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-05-29 14:28:07 -0300

    ajsute

--- app.js
+++ app.js
@@ -14,28 +14,6 @@ app.use(routes);
 app.use(routerReports);
 
 
-app.get('/api/relatorio', (req, res) => {
-    const content = `
-    <div style="display: flex">
-        <h1>Conteúdo do PDF</h1>
-        <h1>Conteúdo do PDF</h1>
-    </div>
-    `;
-
-    pdf.create(content).toStream((err, stream) => {
-        if (err) {
-            console.error('Erro ao gerar o PDF:', err);
-            res.status(500).send('Erro ao gerar o PDF');
-            return;
-        }
-
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', 'inline; filename=relatorio.pdf');
-
-        stream.pipe(res);
-    });
-});
-
 // Rota para fornecer o arquivo PDF do relatório
 app.listen(3333, () => {
     console.log(`Server is running on port 3333`);
--- reports/index.js
+++ reports/index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const routerReports = express.Router();
 const urlBase = '/api';
 
-// // Fornecedor
+// Fornecedor
 const { reportFornecedor } = require('./fornecedor/generate');
-routerReports.get(`${urlBase}/relatorio/fornecedor/`, reportFornecedor);
+routerReports.post(`${urlBase}/relatorio/fornecedor/`, reportFornecedor);
 
 // Recebimento MP
 const { reportRecebimentoMP } = require('../reports/recebimentoMP/generate');
 routerReports.post(`${urlBase}/relatorio/recebimentoMP/`, reportRecebimentoMP);
 
-//teste
-const { teste } = require('../reports/teste/generate');
-routerReports.post(`${urlBase}/teste`, teste);
 
 module.exports = routerReports;
