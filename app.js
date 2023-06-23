5b80ebbd4e34447b4b1c66e5ff8c852f1519b023
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-06-23 15:01:25 -0300

    teste relatório

--- app.js
+++ app.js
@@ -1,116 +1,79 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const puppeteer = require('puppeteer');
-const fs = require('fs');
 const routes = require("./routes");
 const routerReports = require("./reports");
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 app.use(routes);
 app.use(routerReports);
-
-
 app.use('/api/uploads', express.static('uploads'));
 
+const pdf = require('html-pdf');
+const fs = require('fs');
 
-// Rota para criar o PDF
-app.post('/api/pdf', async (req, res) => {
-  try {
-    await createPdf();
+app.get('/api/gerar-relatorio', (req, res) => {
+    const html = `
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
 
-    res.sendStatus(200);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+        .report-table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+        
+        .report-table th, .report-table td {
+            border: 1px solid black;
+            padding: 8px;
+        }
+        
+        .report-table thead {
+            background-color: #f2f2f2;
+        }
+        
+        .report-title {
+            margin-top: 20px;
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+    <h1 class="report-title">Relatório</h1>
+    <table class="report-table">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Idade</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>João</td>
+                <td>30</td>
+            </tr>
+            <tr>
+                <td>Maria</td>
+                <td>25</td>
+            </tr>
+        </tbody>
+    </table>
+`;
+
+
+    // Gere o PDF a partir do HTML
+    pdf.create(html).toStream((err, stream) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erro ao gerar o PDF');
+        } else {
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'attachment; filename=relatorio.pdf');
+
+            // Envie o relatório PDF como resposta
+            stream.pipe(res);
+        }
+    });
 });
-
-// Rota para fazer o download do PDF
-app.get('/api/pdf/download', (req, res) => {
-  const filePath = 'report.pdf';
-
-  if (fs.existsSync(filePath)) {
-    res.download(filePath);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-// Função para criar o PDF usando Puppeteer
-async function createPdf() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.setContent(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    </head>
-    <body>
-      <div class="p-4 bg-blue-500 text-white">
-        Hello, World!
-      </div>
-    </body>
-    </html>
-  `);
-
-  await page.pdf({ path: 'report.pdf' });
-
-  // await browser.close();
-}
-
-
-// Gerar pdf
-
-
-
-
-
-
-// Rota para criar o PDF
-app.post('/api/pdf/gerar', async (req, res) => {
-  try {
-    const pdfBuffer = await createPdf();
-
-    // Define o cabeçalho Content-Type para indicar que é um arquivo PDF
-    res.setHeader('Content-Type', 'application/pdf');
-
-    // Envia o buffer do PDF como resposta
-    res.send(pdfBuffer);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
-
-// Função para criar o PDF usando Puppeteer
-async function createPdf() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.setContent(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    </head>
-    <body>
-      <div class="p-4 bg-blue-500 text-white">
-        Hello, World!yttytytyytytyt
-      </div>
-    </body>
-    </html>
-  `);
-
-  const pdfBuffer = await page.pdf();
-
-  await browser.close();
-
-  return pdfBuffer;
-}
 
 app.listen(3333, () => {
-  console.log('Server is running on port 3333');
+    console.log('Server is running on port 3333');
 });
