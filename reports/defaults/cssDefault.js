2ee79bbd6874c34271c58044d509c78636b8ee12
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-06-26 10:39:14 -0300

    aa

--- reports/defaults/cssDefault.js
+++ reports/defaults/cssDefault.js
@@ -1,42 +1,55 @@
 const cssDefault = () => {
-    // cor padrão do sistema
     let cssDefault = `
-    <head>
+      <head>
         <style>
-            *{
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style: none;
-                text-decoration: none;
-            }
-            body {
-                margin: 10px 7px;
-                font-family: roboto, sans-serif;
-            }
-            .title{
-                text-color: #27272A;
-                font-size: 26px;
-                font-weight: bold;
-                text-align: center;
-            }
-            /* Estilos da tabela */
-            .custom-table {
-                border-collapse: collapse;
-                width: 100%;
-            }
-
-            .custom-table thead {
-                background-color: #f2f2f2;
-            }
-
-            .custom-table th,
-            .custom-table td {
-                border: 1px solid black;
-                padding: 8px;
-            }
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            list-style: none;
+            text-decoration: none;
+          }
+          body {
+            margin: 40px 30px 10px 30px;
+            font-family: roboto, sans-serif;
+          }
+          .title {
+            color: #27272A;
+            font-size: 26px;
+            font-weight: bold;
+            text-align: center;
+          }
+          .divider {
+            border-top: 1px solid #e5e7eb;
+          }
+          .titleValues {
+            font-size: 10px;
+            font-weight: bold;
+            color: #27272A;
+          }
+          .values {
+            font-size: 12px;
+            color: #4B4B4B;
+          }
+          .table {
+            margin-top: 20px;
+            width: 100%;
+            margin-bottom: 20px;
+            border-collapse: collapse;
+            border-spacing: 0;
+            font-size: 14px; /* Aumente o tamanho da fonte para 14px */
+          }
+          .table th,
+          .table td {
+            border: 1px solid #e5e7eb;
+            padding: 8px;
+            text-align: left;
+          }
+          .table th {
+            background-color: #f1f1f1;
+          }
         </style>
-    </head>
+      </head>
     `;
     return cssDefault;
 };
--- reports/formularios/fornecedor/dadosFornecedorContent.js
+++ reports/formularios/fornecedor/dadosFornecedorContent.js
@@ -1,34 +1,86 @@
 const cssDefault = require('../../defaults/cssDefault');
 
 const dadosFornecedorContent = (result) => {
-    console.log("🚀 ~ result:", result)
+    console.log("🚀 ~ result:", result);
     let html = `
     <html>`;
-    // CSS
+    //! CSS
     html += cssDefault();
     html += `
         <body>
             <main>
-                <h1 class="title">Dados do fornecedor ${result.fornecedorID}</h1>
+                <h1 class="title" style="padding-bottom: 40px;">Dados do fornecedor</h1>
+                <div class="divider"></div>
                 <table style="width: 100%;">
-                    <tr>`
-    let count = 0
-    result.fields.forEach((element, index) => {
+                    <tr>`;
+    let count = 0;
+    //! Laço dos dados dinâmicos do fornecedor
+    result.fields.forEach((item, index) => {
         html += `
-        <td >ccc</td>`
-
-        //* Validação pra quebrar linha depois da 3ª coluna
-        if (count == 2) { html += `</tr><tr><td colspan="3" style="border-top: 1px solid black;"></td></tr><tr>`; count = -1; } count++;
+                        <td style="padding-bottom: 4px;">
+                            <span class="titleValues">${item.title}</span><br/>
+                            <span class="values">${item.value ?? '--'}</span>
+                        </td>`;
+        //* Validação para quebrar a linha depois da 3ª coluna
+        if (count == 2) {
+            html += `
+                    </tr>
+                        <tr>
+                            <td colspan="3" style="border-top: 1px solid #e5e7eb;"></td>
+                        </tr>
+                    <tr>`;
+            count = -1;
+        }
+        count++;
     });
     html += `
-                        
                     </tr>
                 </table>
+                <div class="divider"></div>`;
+    //! Laço das atividades e sistemas de qualidade
+    html += `
+                <div style="padding-top: 4px;">
+                    <p class="titleValues">Atividades: <span class="values">${result.atividades}</span></p>
+                    <p class="titleValues" style="padding-top: 6px;">Sistema de qualidade: <span class="values">${result.sistemaQualidade ?? '--'}</span></p>
+                </div>`;
+
+    //! Laço dos blocos
+    result.blocos.forEach((bloco) => {
+        html += `
+      <table class="table">
+        <thead>
+          <tr>
+            <th>${bloco.nome}</th>
+            <th>Resposta</th>
+            <th>Observações</th>
+          </tr>
+        </thead>
+        <tbody>`;
+        //! Itens dos blocos
+        bloco.itens.forEach((item) => {
+            html += `
+        <tr>
+          <td>
+            <span>${item.ordem} - </span>${item.nome}
+          </td>
+          <td>${item.resposta ? item.resposta : ''}</td>
+          <td>${item.obsResposta ? item.obsResposta : ''}</td>
+        </tr>`;
+        });
+        html += ` 
+        </tbody>
+      </table>`;
+    });
+
+    //! Assinatura Rodapé
+    html += `
+            <div style="padding-top: 40px;">
+                <h1 class="title" style="font-size: 16px; width: 50%; margin: 0 auto; border-top: 1px solid black">Assinatura do profissional</h1>
+            </div>
             </main>
         </body>
-    </html>
-    `;
+    </html>`;
     return html;
-}
+};
 
 module.exports = dadosFornecedorContent;
