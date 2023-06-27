47b636fee1443eb30056e9aa9480a2b4949adee5
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-06-27 10:29:46 -0300

    teste css no arquivo

--- reports/formularios/fornecedor/content.js
+++ reports/formularios/fornecedor/content.js
@@ -4,9 +4,60 @@ const content = (result) => {
     // console.log("🚀 ~ result:", result);
     let html = `
     <html>`;
-    //! CSS
-    html += cssDefault();
+    // //! CSS
+    // html += cssDefault();
     html += `
+    <head>
+        <style>
+          * {
+            box-sizing: border-box;
+            list-style: none;
+            text-decoration: none;
+          }
+          
+          body {
+            font-family: 'Roboto', sans-serif;
+            color: #333333;
+          }
+          .title {
+            font-size: 28px;
+            font-weight: bold;
+            text-align: center;
+            margin-bottom: 20px;
+          }
+          .divider {
+            border-top: 1px solid #e5e7eb;
+          }
+          .titleValues {
+            font-size: 12px;
+            font-weight: bold;
+            color: #777777;
+            margin-bottom: 8px;
+          }
+          .values {
+            font-size: 14px;
+            color: #222222;
+          }
+          .table {
+            margin-top: 20px;
+            width: 100%;
+            margin-bottom: 20px;
+            border-collapse: collapse;
+            border-spacing: 0;
+            font-size: 14px;
+          }
+          .table th,
+          .table td {
+            border: 1px solid #e5e7eb;
+            padding: 12px;
+            text-align: left;
+          }
+          .table th {
+            background-color: #f1f1f1;
+            font-weight: bold;
+          }
+        </style>
+      </head>
         <body>
             <main>
                 <h1 class="title">Dados do fornecedor</h1>
--- reports/index.js
+++ reports/index.js
@@ -2,123 +2,123 @@ const express = require('express');
 const routerReports = express.Router();
 const urlBase = '/api/relatorio';
 
-const pdf = require('html-pdf-node');
-const fs = require('fs');
+// const pdf = require('html-pdf-node');
+// const fs = require('fs');
 
 const Fornecedor = require('./formularios/fornecedor/generate');
 routerReports.post(`${urlBase}/fornecedor/dadosFornecedor`, Fornecedor);
 
-routerReports.post(`${urlBase}/fornecedor/dadosFornecedor/teste`, async (req, res) => {
-    const html = `
-    <html>
-      <head>
-        <style>
-          @page {
-            margin: 2cm; /* Margens de 2 centímetros em todas as direções */
-          }
+// routerReports.post(`${urlBase}/fornecedor/dadosFornecedor/teste`, async (req, res) => {
+//     const html = `
+//     <html>
+//       <head>
+//         <style>
+//           @page {
+//             margin: 2cm; /* Margens de 2 centímetros em todas as direções */
+//           }
 
-          header {
-            position: fixed; /* Mantém o cabeçalho fixo em todas as páginas */
-            top: 1cm;
-            left: 2cm;
-            right: 2cm;
-          }
+//           header {
+//             position: fixed; /* Mantém o cabeçalho fixo em todas as páginas */
+//             top: 1cm;
+//             left: 2cm;
+//             right: 2cm;
+//           }
 
-          footer {
-            position: fixed; /* Mantém o rodapé fixo em todas as páginas */
-            bottom: 1cm;
-            left: 2cm;
-            right: 2cm;
-            text-align: center;
-          }
+//           footer {
+//             position: fixed; /* Mantém o rodapé fixo em todas as páginas */
+//             bottom: 1cm;
+//             left: 2cm;
+//             right: 2cm;
+//             text-align: center;
+//           }
 
-          footer::after {
-            content: counter(page) " de " counter(pages); /* Insere a numeração da página */
-          }
-        </style>
-      </head>
-      <body>
-        <header>
-          <h1>Cabeçalho do Relatório</h1>
-        </header>
-        <main>
-          <h2>Relatório</h2>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-            <p>Conteúdo do relatório</p>
-        </main>
-        <footer></footer>
-      </body>
-    </html>
-  `;
+//           footer::after {
+//             content: counter(page) " de " counter(pages); /* Insere a numeração da página */
+//           }
+//         </style>
+//       </head>
+//       <body>
+//         <header>
+//           <h1>Cabeçalho do Relatório</h1>
+//         </header>
+//         <main>
+//           <h2>Relatório</h2>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//             <p>Conteúdo do relatório</p>
+//         </main>
+//         <footer></footer>
+//       </body>
+//     </html>
+//   `;
 
-    const options = {
-        format: 'A4',
-        margin: {
-            top: '2cm',
-            right: '2cm',
-            bottom: '2cm',
-            left: '2cm',
-        },
-        displayHeaderFooter: true,
-        headerTemplate: '<div></div>',
-        footerTemplate: '<div style="text-align: center; font-size: 10px;"><span class="pageNumber"></span> de <span class="totalPages"></span></div>',
-    };
+//     const options = {
+//         format: 'A4',
+//         margin: {
+//             top: '2cm',
+//             right: '2cm',
+//             bottom: '2cm',
+//             left: '2cm',
+//         },
+//         displayHeaderFooter: true,
+//         headerTemplate: '<div></div>',
+//         footerTemplate: '<div style="text-align: center; font-size: 10px;"><span class="pageNumber"></span> de <span class="totalPages"></span></div>',
+//     };
 
-    const file = { content: html };
+//     const file = { content: html };
 
-    try {
-        const pdfBuffer = await pdf.generatePdf(file, options);
-        res.set({
-            'Content-Type': 'application/pdf',
-            'Content-Disposition': 'inline; filename="relatorio.pdf"',
-        });
-        res.send(pdfBuffer);
-    } catch (error) {
-        console.error('Erro ao gerar o relatório PDF:', error);
-        res.status(500).send('Erro ao gerar o relatório PDF.');
-    }
-});
+//     try {
+//         const pdfBuffer = await pdf.generatePdf(file, options);
+//         res.set({
+//             'Content-Type': 'application/pdf',
+//             'Content-Disposition': 'inline; filename="relatorio.pdf"',
+//         });
+//         res.send(pdfBuffer);
+//     } catch (error) {
+//         console.error('Erro ao gerar o relatório PDF:', error);
+//         res.status(500).send('Erro ao gerar o relatório PDF.');
+//     }
+// });
 
 module.exports = routerReports;
