d2069e0f160497bbdae891f906563e7a1871eee7
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-07-03 10:46:19 -0300

    sss

--- reports/index.js
+++ reports/index.js
@@ -6,45 +6,45 @@ const Fornecedor = require('./formularios/fornecedor/generate');
 routerReports.post(`${urlBase}/fornecedor/dadosFornecedor`, Fornecedor);
 
 
-//! pdfkit
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
-const pdf = require('html-pdf');
+// //! pdfkit /  meçlhor opção fora Puppeteer
+// const PDFDocument = require('pdfkit');
+// const fs = require('fs');
+// const pdf = require('html-pdf');
 
-routerReports.post(`/teste2`, (req, res) => {
-  const relatorioHTML = `
-    <html>
-      <head>
-        <title>Relatório</title>
-        <style>
-          /* Estilos CSS para o relatório */
-        </style>
-      </head>
-      <body>
-        <h1>Relatório</h1>
-        <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
-        <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
-        <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
-        <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
-        <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
-        <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
-        <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
+// routerReports.post(`/teste2`, (req, res) => {
+//   const relatorioHTML = `
+//     <html>
+//       <head>
+//         <title>Relatório</title>
+//         <style>
+//           /* Estilos CSS para o relatório */
+//         </style>
+//       </head>
+//       <body>
+//         <h1>Relatório</h1>
+//         <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
+//         <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
+//         <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
+//         <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
+//         <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
+//         <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
+//         <p>euuuithjrtuhrtjkrthnjkrtnçm,nrjknfgjkrgnçjkgn gm,ef gmw çtklrentjrçntjkrtnmrlçkjrtrtrt</p>
 
-        <!-- Mais conteúdo do relatório -->
-      </body>
-    </html>
-  `;
+//         <!-- Mais conteúdo do relatório -->
+//       </body>
+//     </html>
+//   `;
 
-  // Gerar o PDF a partir do HTML usando o html-pdf
-  pdf.create(relatorioHTML).toStream((err, stream) => {
-    // Definir o cabeçalho da resposta com o PDF do relatório
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename="relatorio.pdf"');
+//   // Gerar o PDF a partir do HTML usando o html-pdf
+//   pdf.create(relatorioHTML).toStream((err, stream) => {
+//     // Definir o cabeçalho da resposta com o PDF do relatório
+//     res.setHeader('Content-Type', 'application/pdf');
+//     res.setHeader('Content-Disposition', 'attachment; filename="relatorio.pdf"');
 
-    // Transmitir o conteúdo do PDF para a resposta HTTP
-    stream.pipe(res);
-  });
-});
+//     // Transmitir o conteúdo do PDF para a resposta HTTP
+//     stream.pipe(res);
+//   });
+// });
 
 
 
@@ -57,7 +57,7 @@ routerReports.post(`/teste2`, (req, res) => {
 
 //! Puppeteer
 const puppeteer = require('puppeteer');
-routerReports.post(`${urlBase}/fornecedor/dadosFornecedor/teste`, async (req, res) => {
+routerReports.post(`/teste2`, async (req, res) => {
   const chromiumPath = '/home/gedagroc/.local/bin:/home/gedagroc/bin:/usr/local/cpanel/3rdparty/lib/path-bin:/usr/share/Modules/bin:/usr/local/cpanel/3rdparty/lib/path-bin:/usr/local/jdk/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/X11R6/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin:/opt/bin:/opt/cpanel/composer/bin';
   // Inicialize o Puppeteer
   // const browser = await puppeteer.launch({ executablePath: chromiumPath });
