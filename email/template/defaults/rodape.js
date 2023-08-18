347f9e0972ef906484d877c7ea29b2dc470829c7
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-08-18 09:46:13 -0300

    ajsutes merge

--- email/template/defaults/rodape.js
+++ email/template/defaults/rodape.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-const rodape = () => {
-=======
 const rodape = (values) => {
->>>>>>> origin/master
     let html = `
     <div class="box rodape">
         <p class="textSmall">Este é um e-mail automático, não responda.</p>
         <div>
-<<<<<<< HEAD
-            <a  href="https://gedagro.com.br/" class="superSmall">Gedagro - Gerenciamento Eletrônico de Documentos</a><br/>
-            <span class="superSmall">Rua Minas Gerais, 533, Sala 206 - 89801-200 - Chapecó - SC</span>
-=======
             <p class="superSmall">${values.nomeFabricaSolicitante}</p>
             <span class="superSmall">${values.enderecoCompletoFabricaSolicitante}</span>
->>>>>>> origin/master
         </div>
     </div>`;
     return html;
