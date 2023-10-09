e461035bd386cfca53dd5c531ce790a697681d14
jonatanK28 <jonatankalmeidakk28@gmail.com>
2023-10-09 16:46:40 -0300

    aa

--- reports/layouts/headerReport.js
+++ reports/layouts/headerReport.js
@@ -2,17 +2,18 @@
 const db = require('../../config/db');
 
 const headerReport = async (req, res) => {
-    let { id, unidadeID, isFornecedor } = req.body
+    let data = req.body
+    let unidadeID = data.unidadeID
 
     //? Se fornecedor: Obtém unidadeID da fábrica (quem define o padrão do formulário)
-    if (isFornecedor) {
+    if (data.isFornecedor) {
         const sqlUnity = `SELECT *, unidadeID FROM fornecedor WHERE fornecedorID = ? LIMIT 1`;
-        const [resultUnidade] = await db.promise().query(sqlUnity, id);
+        const [resultUnidade] = await db.promise().query(sqlUnity, [data.fornecedorID]);
         unidadeID = resultUnidade[0]['unidadeID'] //? unidadeID da fábrica
     }
 
     const sqlGetCabecalhoReport = 'SELECT tituloRelatorio, cabecalhoRelatorio FROM unidade WHERE unidadeID = ?'
-    const [resultSqlGetCabecalhoReport] = await db.promise().query(sqlGetCabecalhoReport, unidadeID);
+    const [resultSqlGetCabecalhoReport] = await db.promise().query(sqlGetCabecalhoReport, [unidadeID]);
 
     if (resultSqlGetCabecalhoReport.length <= 0) return
 
