90acf8eb5856c87e1fedac4f42467bb8562d86d6
Roberto Delavi <roberto.delavy@gmail.com>
2023-10-11 14:51:28 -0300

    ...

--- controllers/configuracoes/formularios/fornecedor/fornecedorController.js
+++ controllers/configuracoes/formularios/fornecedor/fornecedorController.js
@@ -158,7 +158,7 @@ class FornecedorController {
 
             //? Header
             header && header.forEach(async (item) => {
-                if (item && item.mostra == true/*&& (item.mostra || item.mostra == 1)*/) {
+                if (item && item.obrigatorio == true/*&& (item.mostra || item.mostra == 1)*/) {
                     // Verifica se já existe registro em "par_fornecedor_unidade" para o fornecedor e unidade
                     const sqlHeader = `
                     SELECT COUNT(*) AS count
