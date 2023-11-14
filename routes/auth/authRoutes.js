cf06fbaf5051527f75bfb107450c8edc019cb3d4
robertodelavi <roberto.delavy@gmail.com>
2023-11-14 11:17:01 -0300

    Funcionando atualizar todos os itens da coluna

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -4,10 +4,10 @@ const authRoutes = Router();
 const AuthController = require('../../controllers/auth/authController');
 const authController = new AuthController();
 
-const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const { configureMulterMiddleware } = require('../../config/uploads');
+// const multer = require('multer');
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
+// const { configureMulterMiddleware } = require('../../config/uploads');
 
 //* Login da fábrica
 const route = '/login';
@@ -19,7 +19,7 @@ authRoutes.post(`${routeForgotPassword}/validation`, authController.routeForgotE
 authRoutes.post(`${routeForgotPassword}/newPassword`, authController.routeForgotNewPassword);
 
 //* TESTE FOTOS
-authRoutes.post(`${route}/testeFoto`, authController.testeFoto);
+// authRoutes.post(`${route}/testeFoto`, authController.testeFoto);
 // authRoutes.post(`${route}/enviaFoto`, authController.enviaFoto);
 // authRoutes.post(`${route}/enviaFoto`, upload.array('files[]'), (req, res) => {
 //     const fotos = req.files; // O arquivo estará disponível em req.file
@@ -27,10 +27,10 @@ authRoutes.post(`${route}/testeFoto`, authController.testeFoto);
 
 // }, authController.enviaFoto);
 
-authRoutes.post(`${route}/enviaFoto`, (req, res, next) => {
-    console.log('route: ', req.files)
-    const pathDestination = `uploads/0/fornecedor/teste/`
-    configureMulterMiddleware(req, res, next, 1, 1, pathDestination)
-}, authController.enviaFoto);
+// authRoutes.post(`${route}/enviaFoto`, (req, res, next) => {
+//     console.log('route: ', req.files)
+//     const pathDestination = `uploads/0/fornecedor/teste/`
+//     configureMulterMiddleware(req, res, next, 1, 1, pathDestination)
+// }, authController.enviaFoto);
 
 module.exports = authRoutes;
