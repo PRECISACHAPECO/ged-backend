d1cf932e656d7650838a6ab684ddb809cb3e8fc3
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-12-14 11:06:37 -0300

    aa

--- reports/formularios/recebimentoMp/dadosRecebimentoMp/dynamic.js
+++ reports/formularios/recebimentoMp/dadosRecebimentoMp/dynamic.js
@@ -1,122 +1,174 @@
 const db = require('../../../../config/db');
 
-const dynamic = async (data) => {
+const dynamic = async (data, modelo) => {
+    const resultData = [];
 
-    sqlRecebimentoMp = `
+    // Fields fixos
+    sqlRecebimento = `
     SELECT 
-    a.*,
-    b.nome AS quemAbriu,
-    c.nome AS aprovaProfissional,
-    d.nome AS finalizaProfissional,
-    e.nome AS nomeFantasia,
-    DATE_FORMAT(a.dataInicio, '%d/%m/%Y') AS dataInicio,
-    DATE_FORMAT(a.dataInicio, '%H:%i:%s') AS horaInicio,
-    DATE_FORMAT(a.dataFim, '%d/%m/%Y') AS dataFim,
-    DATE_FORMAT(a.dataFim, '%H:%i:%s') AS horaFim    
-FROM recebimentomp AS a
-LEFT JOIN profissional AS b ON (a.abreProfissionalID = b.profissionalID)
-LEFT JOIN profissional AS c ON (a.aprovaProfissionalID = c.profissionalID)
-LEFT JOIN profissional AS d ON (a.finalizaProfissionalID = d.profissionalID)
-LEFT JOIN fornecedor AS e ON (a.fornecedorID = e.fornecedorID)
+        a.*,
+        b.nome AS quemAbriu,
+        c.nome AS aprova,
+        d.nome AS preenche,
+        e.nome AS nomeFantasia,
+        DATE_FORMAT(a.dataInicio, '%d/%m/%Y') AS dataInicio,
+        DATE_FORMAT(a.dataInicio, '%H:%i:%s') AS horaInicio,
+        DATE_FORMAT(a.dataFim, '%d/%m/%Y') AS dataFim,
+        DATE_FORMAT(a.dataFim, '%H:%i:%s') AS horaFim    
+    FROM recebimentomp AS a
+    LEFT JOIN profissional AS b ON (a.abreProfissionalID = b.profissionalID)
+    LEFT JOIN profissional AS d ON (a.preencheProfissionalID = d.profissionalID)
+    LEFT JOIN profissional AS c ON (a.aprovaProfissionalID = c.profissionalID)
+    JOIN fornecedor AS e ON (a.fornecedorID = e.fornecedorID)
 
-WHERE a.recebimentoMpID = ?;
-`
-    const [resultSqlRecebimentoMp] = await db.promise().query(sqlRecebimentoMp, [data.id])
-    const resultData = resultSqlRecebimentoMp[0]
+    WHERE a.recebimentoMpID = ?`
+    const [resultSqlRecebimentoMp] = await db.promise().query(sqlRecebimento, [data.id])
+    const resultDataFixos = resultSqlRecebimentoMp[0]
 
     const header = [
         {
             'name': 'Data Inicio',
-            'value': resultData.dataInicio
+            'value': resultDataFixos.dataInicio
         },
         {
             'name': 'Data Fim',
-            'value': resultData.dataFim
+            'value': resultDataFixos.dataFim
         },
         {
             'name': 'Hora Inicio',
-            'value': resultData.horaInicio
+            'value': resultDataFixos.horaInicio
         },
         {
             'name': 'Hora Fim',
-            'value': resultData.horaFim
+            'value': resultDataFixos.horaFim
         },
         {
-            'name': 'Nome Fantasia',
-            'value': resultData.nomeFantasia
+            'name': 'Fornecedor',
+            'value': resultDataFixos.nomeFantasia
         },
         {
-            'name': 'Abre Profissional',
-            'value': resultData.abreProfissional
+            'name': 'Profissional que abriu',
+            'value': resultDataFixos.quemAbriu
         },
         {
-            'name': 'Finaliza Profissional',
-            'value': resultData.finalizaProfissional
+            'name': 'Profissional que preenche',
+            'value': resultDataFixos.preenche
         },
         {
-            'name': 'Aprova Profissional',
-            'value': resultData.aprovaProfissional
+            'name': 'Profissional que aprova',
+            'value': resultDataFixos.aprova
         },
-        {
-            'name': 'Nome Fantasia',
-            'value': resultData.nomeFantasia
-        },
-        {
-            'name': 'CNPJ',
-            'value': resultData.cnpj
-        },
-        {
-            'email': 'Email',
-            'value': resultData.email
-        },
-        {
-            'name': 'Observação',
-            'value': resultData.obs,
-        },
-        {
-            'name': 'CEP',
-            'value': resultData.cep
-
-        },
-        {
-            'name': 'Rua',
-            'value': resultData.logradouro
-        },
-        {
-            'name': 'Numero',
-            'value': resultData.numero
-        },
-        {
-            'name': 'Complemento',
-            'value': resultData.complemento
-        },
-        {
-            'name': 'Bairro',
-            'value': resultData.bairro
-        },
-        {
-            'name': 'Municipio',
-            'value': resultData.cidade
-        },
-        {
-            'name': 'UF',
-            'value': resultData.estado
-        },
-        {
-            'name': 'DYNAMIC',
-            'value': 'TESTEEEE'
-        },
-
     ]
 
 
+    resultData.push(...header)
+
+    // Fields header dinamicos
+    const sqlRecebimentoFixos = `
+    SELECT *
+    FROM par_recebimentomp AS pf 
+        LEFT JOIN par_recebimentomp_modelo_cabecalho AS pfmc ON (pf.parRecebimentoMpID = pfmc.parRecebimentoMpID)
+    WHERE pfmc.parRecebimentoMpModeloID = ?
+    ORDER BY pfmc.ordem ASC`
+    const [colunsRecebimento] = await db.promise().query(sqlRecebimentoFixos, [modelo]);
+
+    const columns = colunsRecebimento.map(row => row.nomeColuna);
+    const titleColumns = colunsRecebimento.map(row => row.nomeCampo);
+    const titleTable = colunsRecebimento.map(row => row.tabela);
+    const typeColumns = colunsRecebimento.map(row => row.tipo);
+
+
+    for (let i = 0; i < columns.length; i++) {
+        let sqlQuery = ''
+        if (typeColumns[i] == 'int') {
+            sqlQuery = `
+            SELECT  
+                b.nome 
+            FROM recebimentomp AS a 
+                JOIN ${titleTable[i]} AS b ON(a.${columns[i]} = b.${columns[i]})  
+            WHERE recebimentoMpID = ?`
+        } else {
+            sqlQuery = `SELECT  ${columns[i]} FROM recebimentomp WHERE recebimentoMpID = ?`;
+        }
+        const [queryResult] = await db.promise().query(sqlQuery, [data.id]);
+        console.log("🚀 ~ queryResult:", queryResult)
+
+        resultData.push({
+            name: titleColumns[i],
+            value: typeColumns[i] === 'date' && queryResult[0][columns[i]] !== null
+                ? new Date(queryResult[0][columns[i]]).toLocaleDateString('pt-BR')
+                : typeColumns[i] === 'int'
+                    ? queryResult[0]?.nome
+                    : queryResult[0][columns[i]]
+
+        });
+    }
+
+    // Blocos e itens
+    const sqlBlocks = `
+    SELECT 
+        nome, 
+        parRecebimentoMpModeloBlocoID
+    FROM par_recebimentomp_modelo_bloco
+    WHERE parRecebimentoMpModeloID = ? AND status = 1
+    ORDER BY ordem ASC`;
+
+    const [resultSqlBlocks] = await db.promise().query(sqlBlocks, [modelo]);
+    const blocks = [];
+
+    for (const block of resultSqlBlocks) {
+        const blockID = block.parRecebimentoMpModeloBlocoID;
+
+        const sqlItensBlock = `
+        SELECT 
+            i.nome,
+            (SELECT fr.resposta
+            FROM recebimentomp_resposta AS fr 
+            WHERE fr.recebimentoMpID = ? AND fr.parRecebimentoMpModeloBlocoID = pfbi.parRecebimentoMpModeloBlocoID AND fr.itemID = pfbi.itemID) AS resposta
+        FROM par_recebimentomp_modelo_bloco_item AS pfbi 
+            LEFT JOIN item AS i ON(pfbi.itemID = i.itemID)
+        WHERE pfbi.parRecebimentoMpModeloBlocoID = ? AND pfbi.status = 1
+        ORDER BY pfbi.ordem ASC`;
+        const [resultSqlItensBlock] = await db.promise().query(sqlItensBlock, [data.id, blockID]);
+
+        blocks.push({
+            nome: block.nome,
+            itens: resultSqlItensBlock,
+        });
+    }
+
+    // Produtos
+    const getProdutos = `
+    SELECT
+        rp.recebimentoMpProdutoID,
+        rp.quantidade,
+        DATE_FORMAT(rp.dataFabricacao, '%Y-%m-%d') AS dataFabricacao,
+        rp.lote,
+        rp.nf,
+        DATE_FORMAT(rp.dataValidade, '%Y-%m-%d') AS dataValidade,
+        p.produtoID,
+        p.nome AS produto,
+        a.apresentacaoID,
+        a.nome AS apresentacao                
+    FROM recebimentomp_produto AS rp
+        JOIN produto AS p ON(rp.produtoID = p.produtoID)
+        JOIN unidademedida AS um ON(p.unidadeMedidaID = um.unidadeMedidaID)
+        LEFT JOIN apresentacao AS a ON(rp.apresentacaoID = a.apresentacaoID)
+    WHERE rp.recebimentoMpID = ?
+    ORDER BY p.nome ASC`
+
+    const [resultProdutos] = await db.promise().query(getProdutos, [data.id]);
+
 
     const values = {
-        header,
+        header: resultData,
+        blocks,
+        produtos: resultProdutos
 
     };
 
 
+    console.log("🚀 ~ values:", values)
     return values
 }
 
