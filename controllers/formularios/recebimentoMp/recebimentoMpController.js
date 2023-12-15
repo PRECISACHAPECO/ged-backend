5e2ed256054a58af1b11ca481829b54a3eba0888
robertodelavi <roberto.delavy@gmail.com>
2023-12-15 16:36:39 -0300

    Teste email nc

--- controllers/formularios/recebimentoMp/recebimentoMpController.js
+++ controllers/formularios/recebimentoMp/recebimentoMpController.js
@@ -7,6 +7,8 @@ const { addFormStatusMovimentation, formatFieldsToTable, hasUnidadeID } = requir
 const { hasPending, deleteItem, criptoMd5, onlyNumbers, gerarSenha, gerarSenhaCaracteresIniciais, removeSpecialCharts } = require('../../../config/defaultConfig');
 const { executeLog, executeQuery } = require('../../../config/executeQuery');
 const { send } = require('process');
+const fornecedorPreenche = require('../../../email/template/recebimentoMP/naoConformidade/fornecedorPreenche');
+const sendMailConfig = require('../../../config/email');
 
 class RecebimentoMpController {
     async getList(req, res) {
@@ -612,7 +614,7 @@ class RecebimentoMpController {
                 //? Se ainda não enviou email ao fornecedor preencher NC, verifica se precisa enviar
                 // if (result[0]['naoConformidadeEmailFornecedor'] != 1) 
                 // naoConformidadeEmailFornecedor
-                checkNotificationFornecedor(id, data.fieldsHeader.fornecedor, data.naoConformidade.itens, unidadeID, usuarioID, papelID)
+                checkNotificationFornecedor(id, data.fieldsHeader.fornecedor, data.naoConformidade.itens, unidadeID, usuarioID, papelID, req)
             }
 
             //? Gera histórico de alteração de status (se houve alteração)
@@ -1091,7 +1093,7 @@ const getSqlOtherInfos = () => {
     return sql
 }
 
-const checkNotificationFornecedor = async (recebimentoMpID, fornecedor, arrNaoConformidades, unidadeID, usuarioID, papelID) => {
+const checkNotificationFornecedor = async (recebimentoMpID, fornecedor, arrNaoConformidades, unidadeID, usuarioID, papelID, req) => {
     if (arrNaoConformidades.length === 0) return
 
     const arrProducts = []
@@ -1115,16 +1117,112 @@ const checkNotificationFornecedor = async (recebimentoMpID, fornecedor, arrNaoCo
             products: arrProducts ?? []
         }
 
-        const url = `${process.env.BASE_URL_API}formularios/recebimento-mp/nao-conformidade/fornecedor-preenche`
-        const result = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(data),
-        })
+        // const url = `${process.env.BASE_URL_API}formularios/recebimento-mp/nao-conformidade/fornecedor-preenche`
+        // const result = await fetch(url, {
+        //     method: 'POST',
+        //     body: JSON.stringify(data),
+        // })
         // const result = await axios.post(url, data)
 
         //? Atualiza flag de envio de email
         const sqlUpdate = `UPDATE recebimentomp SET naoConformidadeEmailFornecedor = ? WHERE recebimentoMpID = ? `
         const [resultUpdate] = await db.promise().query(sqlUpdate, [1, recebimentoMpID])
+
+        // const data = req.body
+        console.log("🚀 ~ data do email:", data)
+
+        // Dados unidade fabrica
+        const sqlFabrica = `SELECT * FROM unidade WHERE unidadeID = ?`
+        const [result] = await db.promise().query(sqlFabrica, [data.unidadeID])
+
+        //Dados fornecedor
+        const sqlFornecedor = `SELECT * FROM fornecedor WHERE fornecedorID = ?`
+        const [resultFornecedor] = await db.promise().query(sqlFornecedor, [data.fornecedorID])
+
+        const password = gerarSenhaCaracteresIniciais(resultFornecedor[0].cnpj, 4)
+
+        //Dados profissional logado
+        const sqlProfessional = `
+        SELECT 
+            a.nome,
+            b.formacaoCargo AS cargo
+        FROM profissional AS a 
+            LEFT JOIN profissional_cargo AS b ON (a.profissionalID = b.profissionalID)
+        WHERE a.profissionalID = ?`
+        const [resultSqlProfessional] = await db.promise().query(sqlProfessional, [data.usuarioID])
+
+        const values = {
+            // Unidade Fbrica
+            nomeFantasiaFabrica: result[0].nomeFantasia,
+
+            // Unidade Fornecedor
+            nomeFantasia: resultFornecedor[0].nome,
+            razaoSocial: resultFornecedor[0].razaoSocial,
+            cnpjFornecedor: resultFornecedor[0].cnpj,
+            senhaFornecedor: password,
+
+            // profissional que abriu formulario
+            nomeProfissional: resultSqlProfessional[0]?.nome,
+            cargoProfissional: resultSqlProfessional[0]?.cargo,
+
+            // Outros
+            unidadeID: data.unidadeID,
+            usuarioID: data.usuarioID,
+            papelID: data.papelID,
+            fornecedorID: data.fornecedorID,
+            stage: 's3',
+            link: `${process.env.BASE_URL}/fornecedor?r=${data.recebimentoMpID}`,
+            products: data.products
+
+        }
+
+        // Envia email para preencher não conformidade no recebimentoMp 
+        const logID = await executeLog('Email para preencher não conformidade no recebimentoMp', data.usuarioID, data.unidadeID, req)
+        const destinatario = resultFornecedor[0].email
+        let assunto = `GEDagro - Prencher não conformidade `
+        const html = await fornecedorPreenche(values);
+        await sendMailConfig(destinatario, assunto, html, logID, values)
+
+        // Novo fornecedor, envia email como dados de acesso
+        if (!data.isUser) {
+            const logID = await executeLog('Email e criação de novo fornecedor', data.usuarioID, data.unidadeID, req)
+
+            // Verifica se CNPJ já está cadastrado
+            const cnpjExists = "SELECT * FROM usuario WHERE cnpj = ?"
+            const [resultCnpjExists] = await db.promise().query(cnpjExists, [resultFornecedor[0].cnpj])
+
+            if (resultCnpjExists.length > 0) {
+                return
+            } else {
+                // Cadastra novo usuário
+                const sqlNewUuser = `
+                   INSERT INTO usuario(nome, cnpj, email, senha)
+                  VALUES(?, ?, ?, ?)`
+                const usuarioID = await executeQuery(sqlNewUuser, [resultFornecedor[0].nome, resultFornecedor[0].cnpj, resultFornecedor[0].email, criptoMd5(password)], 'insert', 'usuario', 'usuarioID', null, logID)
+                // return
+
+                // Salva a unidade
+                const sqlInsertUnity = `
+                  INSERT INTO unidade (razaoSocial, nomeFantasia, cnpj, email) VALUES (?,?, ?, ?)`
+                const newUnidadeID = await executeQuery(sqlInsertUnity, [resultFornecedor[0].nome, resultFornecedor[0].nome, resultFornecedor[0].cnpj, data.email], 'insert', 'unidade', 'unidadeID', null, logID)
+
+                // Salva usuario_unidade
+                const sqlNewUserUnity = `
+                INSERT INTO usuario_unidade(usuarioID, unidadeID, papelID)
+                VALUES(?, ?, ?)
+                      `
+                await executeQuery(sqlNewUserUnity, [usuarioID, newUnidadeID, 2], 'insert', 'usuario_unidade', 'usuarioUnidadeID', null, logID)
+
+                let assunto = `Bem-vindo ao GEDagro`
+                const html = await instructionsNewFornecedor(values)
+                await sendMailConfig(destinatario, assunto, html, logID, values)
+            }
+        }
+
+        // Atualiza tabela recebimentoMp
+        const sqlUpdateRecebimentoMp = `UPDATE recebimentoMp SET naoConformidadeEmailFornecedor = 1 WHERE recebimentoMpID = ?`
+        await db.promise().query(sqlUpdateRecebimentoMp, [data.recebimentoMpID])
+
     }
 
 }
